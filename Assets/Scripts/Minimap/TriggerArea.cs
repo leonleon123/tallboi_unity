81351e5452862915ea2e306dcfbc955184b707d2
matemato <miocic2@gmail.com>
2020-12-22 20:16:31 +0100

    Minimap for MatejScene

---
+++ Assets/Scripts/Minimap/TriggerArea.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerArea : MonoBehaviour
+{ 
+    public int id;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Entered");
+        EventListener.current.RoomTriggerEnter(id);
+    }
+
+    // If we want to RenderOff
+    /*
+    private void OnTriggerExit(Collider other) {
+        Debug.Log("Exited");
+        DrawMinimap.current.OnTest(id);
+    }
+    */
+}
