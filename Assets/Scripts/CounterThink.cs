9340dc7069e7981d4d4a822d811e0f292b8eb28d
leonleon123 <lele.slav@gmail.com>
2021-01-06 18:33:16 +0100

    added counter, small fixes

---
+++ Assets/Scripts/CounterThink.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CounterThink : MonoBehaviour
+{
+
+    TMP_Text text;
+    HatController hc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<TMP_Text>();
+        hc = GameObject.FindGameObjectWithTag("Player").GetComponent<HatController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = hc.hats.Count.ToString() + "/3";
+        if(hc.hats.Count >= 3)
+        {
+            text.color = Color.green;
+        }
+    }
+}
